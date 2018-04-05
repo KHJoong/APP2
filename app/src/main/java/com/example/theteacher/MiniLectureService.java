6daab16d584b33fe9accc3b34b12e39ac5b9cd6d
KHJoong <get.api.to.app@gmail.com>
2018-04-05 23:26:06 +0900

    중간 저장(2018.04.05)
    1. 영상을 창모드로 보는 기능 구현 중
     > 이어서 볼 수 있도록,
     > 에러나지 않고 다른 영상을 바로 실행할 수 있도록,
     수정해야 함

---
+++ app/src/main/java/com/example/theteacher/MiniLectureService.java
@@ -0,0 +1,231 @@
+package com.example.theteacher;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.FloatMath;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.MediaController;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.widget.VideoView;
+import android.annotation.SuppressLint;
+
+/**
+ * Created by kimhj on 2018-04-05.
+ */
+
+public class MiniLectureService extends Service {
+
+    int returnValue;
+    String lecUrl;
+
+    LayoutInflater inflater;
+    RelativeLayout viewContainer;
+    VideoView vvMiniLecture;
+    ImageButton btnExit;
+
+    WindowManager.LayoutParams wmParams;
+    WindowManager windowManager;
+
+    Handler handler;
+
+
+    private static final int MODE_NONE = 0;
+    private static final int MODE_ONE_TOUCH = 1;
+    private static final int MODE_DOUBLE_TOUCH = 2;
+
+    boolean isSingleTap;
+    int mTouchMode = MODE_NONE;
+    private float START_X;
+    private float START_Y;
+    private float distance;
+    private int PREV_X;
+    private int PREV_Y;
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler = new Handler();
+        lecUrl = intent.getStringExtra("lecUrl");
+
+//        returnValue = START_NOT_STICKY;
+//        returnValue = START_CONTINUATION_MASK;
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        handler = new Handler();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MiniLectureService.this, "창모드로 전환합니다.", Toast.LENGTH_SHORT).show();
+
+                        inflater = LayoutInflater.from(MiniLectureService.this);
+                        viewContainer = (RelativeLayout) inflater.inflate(R.layout.miniview_lecture, null);
+                        vvMiniLecture = (VideoView)viewContainer.findViewById(R.id.vvMiniLecture);
+                        btnExit = (ImageButton)viewContainer.findViewById(R.id.btnExit);
+
+                        windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+
+                        vvMiniLecture.setOnTouchListener(viewTouchListener);
+                        btnExit.setVisibility(View.GONE);
+                        btnExit.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                if(windowManager!=null){
+                                    if(viewContainer!=null) {
+                                        windowManager.removeView(viewContainer);
+                                        viewContainer = null;
+                                    }
+                                    windowManager = null;
+                                }
+                            }
+                        });
+
+                        wmParams = new WindowManager.LayoutParams(
+                                WindowManager.LayoutParams.WRAP_CONTENT,
+                                WindowManager.LayoutParams.WRAP_CONTENT,
+                                WindowManager.LayoutParams.TYPE_PHONE,
+                                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                                PixelFormat.TRANSLUCENT
+                        );
+                        wmParams.gravity = Gravity.CENTER_HORIZONTAL | Gravity.TOP;
+                        wmParams.x = 0;
+                        wmParams.y = 0;
+
+                        // VideoView 영상 실행
+                        vvMiniLecture.setVideoURI(Uri.parse(lecUrl));
+                        vvMiniLecture.start();
+
+                        windowManager.addView(viewContainer, wmParams);
+                    }
+                });
+            }
+        }).start();
+    }
+
+    @Override
+    public void onDestroy() {
+        if(windowManager!=null){
+            if(vvMiniLecture!=null) {
+                windowManager.removeView(vvMiniLecture);
+                vvMiniLecture = null;
+            }
+            windowManager = null;
+        }
+        stopSelf();
+        super.onDestroy();
+    }
+
+    @SuppressLint("FloatMath")
+    private float doubleTouchDistance(MotionEvent event) {
+
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        distance = (float) (Math.sqrt(x * x + y * y));
+
+        //화면 밖으로 나가지 못하도록 Block
+//        if(distance >= mWidth){
+//            distance = mWidth ;
+//            return distance;
+//        }
+
+        return distance;
+    }
+
+    /**
+     * 터치 리스너
+     */
+    private View.OnTouchListener viewTouchListener = new View.OnTouchListener() {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+
+                    isSingleTap = true;
+                    mTouchMode = MODE_ONE_TOUCH;
+                    btnExit.setVisibility(View.INVISIBLE);
+
+                    START_X = event.getRawX();
+                    START_Y = event.getRawY();
+                    PREV_X = wmParams.x;
+                    PREV_Y = wmParams.y;
+
+                    break;
+
+                case MotionEvent.ACTION_MOVE:    // 이동 시
+
+                    if(mTouchMode == MODE_ONE_TOUCH){
+                        int x = (int) (event.getRawX() - START_X);
+                        int y = (int) (event.getRawY() - START_Y);
+
+                        wmParams.x = PREV_X + x;
+                        wmParams.y = PREV_Y + y;
+
+                        windowManager.updateViewLayout(viewContainer, wmParams);
+                    }
+                    if(mTouchMode == MODE_DOUBLE_TOUCH){
+
+                        isSingleTap = false;
+                        float distance = doubleTouchDistance(event);
+
+                        wmParams.width = (int) distance;
+
+                        windowManager.updateViewLayout(viewContainer, wmParams);
+                    }
+                    break;
+
+                case MotionEvent.ACTION_UP:
+
+                    btnExit.setVisibility(View.VISIBLE);
+                    windowManager.updateViewLayout(viewContainer, wmParams);
+                    btnExit.setVisibility(View.VISIBLE);
+                    break;
+
+                case MotionEvent.ACTION_POINTER_DOWN:    // 두 손가락 터치 시
+                    mTouchMode = MODE_DOUBLE_TOUCH;
+
+                    isSingleTap = false;
+
+                    break;
+
+                case MotionEvent.ACTION_POINTER_UP:        // 두 손가락을 떼었을 시
+
+                    isSingleTap = false;
+                    mTouchMode = MODE_NONE;
+                    break;
+            }
+            return true;
+        }
+    };
+
+
+}
--- app/src/main/java/com/example/theteacher/RecordedViewActivity.java
+++ app/src/main/java/com/example/theteacher/RecordedViewActivity.java
@@ -1,12 +1,24 @@
 package com.example.theteacher;
 
+import android.annotation.TargetApi;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 /**
@@ -15,8 +27,12 @@ import android.widget.VideoView;
 
 public class RecordedViewActivity extends AppCompatActivity {
 
+    private static final int FLOATING_PERMISSION_CODE = 1;
+
     // 녹화된 강의를 재생해주는 뷰입니다.
     VideoView vvRecScreen;
+    //
+    Button btnGoMiniLecture;
 
     String recUrl;  // 강의 rtsp 스트리밍을 받을 때 사용되는 url입니다.
 
@@ -31,15 +47,59 @@ public class RecordedViewActivity extends AppCompatActivity {
         setContentView(R.layout.recview_lecture);
 
         vvRecScreen = (VideoView) findViewById(R.id.vvRecScreen);
+        btnGoMiniLecture = (Button) findViewById(R.id.btnGoMiniLecture);
 
         // MainActivity_NowPlaying에서  Intent로 받은 id를 rtsp url의 구분자로 사용합니다.
         Intent it = getIntent();
         recUrl = "http://115.71.232.230/theteacher/uploaded_video/"+it.getStringExtra("repath");
-        Log.i("Recorded", "ViewAct : "+recUrl);
+
+        btnGoMiniLecture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermission();
+            }
+        });
 
         vvRecScreen.setVideoURI(Uri.parse(recUrl));
         vvRecScreen.setMediaController(new MediaController(this));
         vvRecScreen.requestFocus();
         vvRecScreen.start();
+    }
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   // 마시멜로우 이상일 경우
+            if (!Settings.canDrawOverlays(this)) {              // 체크
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, FLOATING_PERMISSION_CODE);
+            } else {
+                Intent it = new Intent(RecordedViewActivity.this, MiniLectureService.class);
+                it.putExtra("lecUrl", recUrl);
+                startService(it);
+                finish();
+            }
+        } else {
+            Intent it = new Intent(RecordedViewActivity.this, MiniLectureService.class);
+            it.putExtra("lecUrl", recUrl);
+            startService(it);
+            finish();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FLOATING_PERMISSION_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                // TODO 동의를 얻지 못했을 경우의 처리
+
+            } else {
+                Intent it = new Intent(RecordedViewActivity.this, MiniLectureService.class);
+                it.putExtra("lecUrl", recUrl);
+                startService(it);
+                finish();
+            }
+        }
     }
 }
